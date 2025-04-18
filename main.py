4ff551bf7ac5b7511f888a09f52a4d9837423e30
HexieB <hexie.highwind@gmail.com>
2025-04-17 23:52:22 -0700

    Add clock tick, Window ttile, reduce system load

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    dt = 0
+    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
-        pygame.time.delay(100)
+        clock.tick(60)
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
