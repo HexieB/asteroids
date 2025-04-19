cfd13a145c5314e5f48e6b728f41394f9470f1bf
HexieB <hexie.highwind@gmail.com>
2025-04-18 21:17:15 -0700

    creating containers for further game objects

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ def main():
     pygame.display.set_caption("Asteroids")
     dt = 0
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000.0
